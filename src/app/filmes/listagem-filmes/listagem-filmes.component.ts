54228f7ee3ed2e3d72d140a5b81f2817e21052e6
Genilton <geniltonbarbosadasilva@gmail.com>
2021-09-03 00:47:59 -0300

    Pesquisa com filtro

--- src/app/filmes/listagem-filmes/listagem-filmes.component.ts
+++ src/app/filmes/listagem-filmes/listagem-filmes.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { debounceTime } from "rxjs/operators";
 import { FilmesService } from 'src/app/core/filmes.service';
+import { ConfigParams } from 'src/app/shared/models/config-params';
 import { Filme } from 'src/app/shared/models/filme';
 
 @Component({
@@ -8,15 +11,44 @@ import { Filme } from 'src/app/shared/models/filme';
     styleUrls: ['./listagem-filmes.component.scss']
 })
 export class ListagemFilmesComponent implements OnInit {
+    readonly semFoto = "https://www2.camara.leg.br/atividade-legislativa/comissoes/comissoes-permanentes/cindra/imagens/sem.jpg.gif/image";
 
-    readonly qtdPagina = 4;
-    pagina = 0;
+    config: ConfigParams = {
+        pagina: 0,
+        limite: 4
+    }
     filmes: Filme[] = [];
+    filtros: FormGroup;
+    
+    generos: string[];
 
-    constructor(private filmesService: FilmesService) { }
 
-    ngOnInit(): void {        
-        this.listarFilmes();
+    constructor(
+        private filmesService: FilmesService,
+        private formBuilder: FormBuilder
+    ) { }
+
+    ngOnInit(): void {      
+        this.filtros = this.formBuilder.group({
+            texto: [''],
+            genero: ['']
+        });
+
+        this.filtros.get("texto").valueChanges
+            .pipe(debounceTime(500))
+            .subscribe((valor: string)=>{
+            this.config.pesquisa = valor;
+            this.resetarListagem();
+        });
+
+        this.filtros.get("genero").valueChanges.subscribe((valor: string)=>{
+            this.config.campo = { tipo: "genero", valor: valor };
+            this.resetarListagem();
+        });
+
+        this.generos = ['Ação', 'Romance', 'Aventura', 'Terror', 'Ficção cientifica', 'Comédia', 'Aventura', 'Drama'];
+
+        this.listarFilmes();        
     }
 
     onScroll(): void {
@@ -25,9 +57,15 @@ export class ListagemFilmesComponent implements OnInit {
 
     private listarFilmes(): void {
         // this.pagina = (this.pagina > 4)? 1: this.pagina+1;
-        this.pagina++;
+        this.config.pagina++;
         this.filmesService
-            .listar(this.pagina, this.qtdPagina)
+            .listar(this.config)
             .subscribe((filmes: Filme[]) => this.filmes.push(...filmes));
     }
+
+    private resetarListagem(): void {
+		this.config.pagina = 0;
+        this.filmes = [];
+        this.listarFilmes();
+	}
 }
